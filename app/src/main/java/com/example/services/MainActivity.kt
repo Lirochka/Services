a5657564d9267afbceed621bc8d61a8f56f8940c
Lira <lir0na@mail.ru>
2023-07-19 22:12:13 +0300

    Worked with JobService, JobScheduler and method enqueue

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.services
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var page = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -32,6 +35,23 @@ class MainActivity : AppCompatActivity() {
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(this,
                 MyIntentService.newIntent(this))
+        }
+
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(false)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+             //   .setPersisted(true)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
